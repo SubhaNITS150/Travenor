384e0fc9cf2f3ef43c50a6d209c4b1a30a452f91
Subhajyoti <subhajyotidey2910@gmail.com>
2026-06-26 16:34:54 +0530

    Added .env changes

--- travenor-ai/services/diarization_service.py
+++ travenor-ai/services/diarization_service.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 import whisperx
+
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
 
 class DiarizationService:
 
     @staticmethod
     def diarize(audio_path: str):
-
         diarize_model = whisperx.DiarizationPipeline(
-            use_auth_token="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",
+            use_auth_token=HF_TOKEN,
             device="cpu"
         )
 
